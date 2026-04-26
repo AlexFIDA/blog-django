598c8ab09363c47e212515e9a0f89407e74a6283
AlexFIDA <aleksandrpapasvili505@gmail.com>
2026-04-26 22:20:13 +0500

    Add and register posts Model

--- post/models.py
+++ post/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField("Заголовок", max_length=100)
+    content = models.TextField()
+    author = models.CharField("Автор", max_length=50)
+    date_create = models.DateTimeField("Дата создания", auto_now_add=True)
+    
+    def __str__(self):
+        formatted_date = self.date_create.strftime("%Y-%m-%d %H:%M")
+        return f'{self.title} by {self.author}, created at {formatted_date}'
+    
+    class Meta:
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
+        
+
