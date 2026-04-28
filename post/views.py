5d5edb9dcaf789af6cc91c3dd1c2a6d704974fa9
AlexFIDA <aleksandrpapasvili505@gmail.com>
2026-04-28 21:38:04 +0500

    Added display of articles on the main page (without styling)

--- post/views.py
+++ post/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Post
+from django.views.generic import ListView
 
 # Create your views here.
+class PostListView(ListView):
+    model = Post
+    template_name = 'post/post_list.html'
+    context_object_name = 'post_list'
+    ordering = ['-date_create']
